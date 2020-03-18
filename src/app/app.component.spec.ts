6ced473cb613c291eab92a599a9774634448596f
fsliti <foued.sliti@prima-solutions.com>
2020-03-18 17:54:10 +0100

    fix name application

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'springAngularPostgresElasticsDaocker'`, () => {
+  it(`should have as title 'springAngularPostgresElasticsdocker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('springAngularPostgresElasticsDaocker');
+    expect(app.title).toEqual('springAngularPostgresElasticsdocker');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to springAngularPostgresElasticsDaocker!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to springAngularPostgresElasticsdocker!');
   });
 });
